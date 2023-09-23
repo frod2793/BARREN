7c93d35b70b5dfd21770427bf6be55f8b0033cb0
frod2793 <tmdwhd0344@naver.com>
2023-09-23 10:48:04 +0900

    리소스 적용 (타이틀 )

--- Assets/Scripts/DonDestroyOnLoad/DonDestory_UI.cs
+++ Assets/Scripts/DonDestroyOnLoad/DonDestory_UI.cs
@@ -7,17 +7,17 @@ using UnityEngine.UI;
 public class DonDestory_UI : MonoBehaviour
 {
     private static DonDestory_UI instance;
-    
-    [Header("- 버튼 목록")]
-    [SerializeField] private Button settingButton;
+
+    [Header("- 버튼 목록")] [SerializeField] private Button settingButton;
     [SerializeField] private Button newsButton;
 
-    [Header("- 팝업 프리펩  목록")]
-    [SerializeField] private GameObject settingPopUp;
+    [Header("- 팝업 프리펩  목록")] [SerializeField]
+    private GameObject settingPopUp;
+
     [SerializeField] private GameObject newsPopUp;
 
     [Header("- 캔버스")] [SerializeField] private GameObject uiCanvas;
-    
+
     void Awake()
     {
         // 이미 인스턴스가 있는 경우, 새 인스턴스를 파괴하고 기존 인스턴스를 유지합니다.
@@ -30,11 +30,12 @@ public class DonDestory_UI : MonoBehaviour
         // 인스턴스가 없는 경우, 현재 오브젝트를 인스턴스로 설정하고 파괴하지 않도록 설정합니다.
         instance = this;
         DontDestroyOnLoad(gameObject);
-        
+
 
         // 씬 전환 이벤트에 대한 리스너 등록
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
+
     void Start()
     {
         Button_Init();
@@ -44,8 +45,6 @@ public class DonDestory_UI : MonoBehaviour
     {
         //Todo:버튼에 리스너를 추가하여 함수 연결 
         settingButton.onClick.AddListener(Func_SettingBtn);
-        
-     
     }
 
     // 씬 전환 이벤트 핸들러
@@ -57,11 +56,11 @@ public class DonDestory_UI : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     private void Func_SettingBtn()
     {
-        // 로드한 프리펩을 인스턴스화
-      //  부모는 현재 게임 오브젝트로 한다 
-        GameObject settingInstance = Instantiate(settingPopUp,uiCanvas.transform);
+        // settingButton.interactable = false;
+        GameObject settingInstance = Instantiate(settingPopUp, uiCanvas.transform);
 
         // 설정 팝업 열기 
         TweenEffect.OpenPopup(settingInstance);
@@ -71,5 +70,4 @@ public class DonDestory_UI : MonoBehaviour
     {
         //todo:공지사항 프리펩 인스테이트 
     }
-   
 }
